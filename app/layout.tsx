a31f7dcdbd0a1f518c12c95fe1028192f0f5052b
Anne Zeng <anne.zeng9@gmail.com>
2026-04-01 00:16:57 -0400

    refresh image cache

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,11 @@ import localFont from 'next/font/local';
 import Nav from '@/components/Nav';
 import './globals.css';
 
-const dmSans = DM_Sans({ subsets: ['latin'], variable: '--font-body', display: 'swap' });
+const dmSans = DM_Sans({
+  subsets: ['latin'],
+  variable: '--font-body',
+  display: 'swap',
+});
 
 const monumentBlack = localFont({
   src: '../public/fonts/PPMonumentExtended-Black.otf',
@@ -27,14 +31,21 @@ export const metadata: Metadata = {
     googleBot: { index: false, follow: false },
   },
   icons: {
-    icon: '/az-logo.png',
-    apple: '/az-logo.png',
+    icon: '/favicon.png',
+    apple: '/favicon.png',
   },
 };
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
-    <html lang="en" className={dmSans.variable + ' ' + monumentBlack.variable + ' ' + monumentRegular.variable}>
+    <html
+      lang="en"
+      className={`${dmSans.variable} ${monumentBlack.variable} ${monumentRegular.variable}`}
+    >
       <body>
         <Nav />
         <main>{children}</main>
